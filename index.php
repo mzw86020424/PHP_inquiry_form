cb53524ffd4d5a66633b01d9dce72aaef2888600
mzw86020424 <mzw86020424@gmail.com>
2021-03-23 16:39:06 +0900

    問合せフォームが完成

--- index.php
+++ index.php
@@ -20,11 +20,40 @@
   <div class="main">
     <div class="contact-form">
       <div class="form-title">お問い合わせ</div>
-      <form action="sent.php" method="post">
+      <form method="post" action="sent.php">
         <div class="form-item">名前</div>
-        <input type="text" name="name"/>
+        <input type="text" name="name">
+
+        <div class="form-item">年齢</div>
+        <select name="age">
+          <option value="未選択">選択してください</option>
+
+          <?php
+            for ($i=6; $i <= 100 ; $i++) { 
+              echo "<option value='{$i}'>{$i}</option>";
+            }
+          ?>
+        </select>
+
+        <div class="form-item">お問い合わせの種類</div>
+        <?php 
+          $types = array('Progateに関するお問い合わせ', 'Progateに対する意見', '採用に関するお問い合わせ', '取材・メディア関連のお問い合わせ', '料金に関するお問い合わせ', 'その他');
+         ?>
+
+        <select name="category">
+          <option value="未選択">選択してください</option>
+          <?php
+            foreach ($types as $type) {
+              echo "<option value='{$type}'>{$type}</option>";
+            }
+          ?>
+        </select>
+        
+        
         <div class="form-item">内容</div>
         <textarea name="body"></textarea>
+
+        <input type="submit" value="送信">
       </form>
     </div>
   </div>
